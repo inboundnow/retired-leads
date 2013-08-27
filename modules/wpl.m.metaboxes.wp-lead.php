41d17c79477917bc0fae5fa5266882dd7bcfd024
Inbound Now <david@inboundnow.com>
2013-08-27 15:16:47 -0700

    gravatar fix was insanely expensive with get_headers turned off for now

--- modules/wpl.m.metaboxes.wp-lead.php
+++ modules/wpl.m.metaboxes.wp-lead.php
@@ -570,14 +570,7 @@ function wpleads_display_metabox_main() {
 			$gravatar = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
 			$gravatar2 = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size_small;
 		
-			$response = get_headers($gravatar);
-			if ($response[0] === "HTTP/1.0 302 Found"){
-    			$gravatar = $url . '/wp-content/plugins/leads/images/gravatar_default_150.jpg';
-    			$gravatar2 = $url . '/wp-content/plugins/leads/images/gravatar_default_50.jpg';		
-			} else {
-				$gravatar = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
-				$gravatar2 = "http://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size_small;
-			}
+	
 
 			?>
 			<div id="lead_image">
