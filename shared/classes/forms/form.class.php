bef0642925ecba913969347b6f009b195ad611c1
Inbound Now <david@inboundnow.com>
2013-09-08 15:29:06 -0700

    email setup and organizing classes

--- shared/classes/forms/form.class.php
+++ shared/classes/forms/form.class.php
@@ -35,6 +35,7 @@ class InboundForms {
         add_action('init', array(__CLASS__, 'register_script'));
         add_action('wp_footer', array(__CLASS__, 'print_script'));
         add_action('wp_footer', array(__CLASS__, 'inline_my_script'));
+        add_action( 'wp_head',  array(__CLASS__, 'send_email'));
     }
 
     // Shortcode params
@@ -84,8 +85,14 @@ class InboundForms {
               var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
               return re.test(email);
           }
-
-      
+		
+		  // Set textarea equal to input width		
+          jQuery(".inbound-now-form textarea").each(function(){
+              var width = $(this).parent().parent().find("input.inbound-email").width();
+              if(width != ""){
+                $(this).width(width);
+              }
+          }); 
 
           // Turn off submit if no email present
           jQuery(".inbound-now-form").each(function(){
@@ -116,6 +123,7 @@ class InboundForms {
           </script>';
 
     echo "<style type='text/css'>
+    	/* Add button style options http://medleyweb.com/freebies/50-super-sleek-css-button-style-snippets/ */
           .inbound-field { margin-bottom:5px; }
           .inbound-label {min-width:90px; display:inline-block;}
           .inbound-label.text-area-on {vertical-align:top;}
@@ -133,55 +141,67 @@ class InboundForms {
     }
     
     static function send_email(){
-      // need action on post
-      /* Not tested from: http://wp.tutsplus.com/tutorials/creative-coding/creating-a-simple-contact-form-for-simple-needs/
-      // if the <form> element is POSTed, run the following code
-        if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-            $error = false;
-            // set the "required fields" to check
-            $required_fields = array( "your_name", "email", "message", "subject" );
-         
-            // this part fetches everything that has been POSTed, sanitizes them and lets us use them as $form_data['subject']
-            foreach ( $_POST as $field => $value ) {
+      // Cross reference with EDD email class for HTML sends
+      // Add PHP processing for lead data
+        if(isset($_POST['inbound_submitted']) && $_POST['inbound_submitted'] === '1') {
+
+          foreach ( $_POST as $field => $value ) {
                 if ( get_magic_quotes_gpc() ) {
                     $value = stripslashes( $value );
                 }
-                $form_data[$field] = strip_tags( $value );
-            }
-         
-            // if the required fields are empty, switch $error to TRUE and set the result text to the shortcode attribute named 'error_empty'
-            foreach ( $required_fields as $required_field ) {
-                $value = trim( $form_data[$required_field] );
-                if ( empty( $value ) ) {
-                    $error = true;
-                    $result = $error_empty;
+                $field = strtolower($field);
+
+                if (preg_match( '/Email|e-mail|email/i', $value)) {
+                $field = "email";
                 }
+
+                if (preg_match( '/(?<!((last |last_)))name(?!\=)/im', $value) && !isset($form_data['first-name'])) {
+                $field = "first-name";
+                }
+
+                if (preg_match( '/(?<!((first)))(last name|last_name|last)(?!\=)/im', $value) && !isset($form_data['last-name'])) {
+                $field = "last-name";
+                }
+
+                if (preg_match( '/Phone|phone number|telephone/i', $value)) {
+                $field = "phone";
+                }
+
+                $form_data[$field] = strip_tags( $value );
+
             }
-         
-            // and if the e-mail is not valid, switch $error to TRUE and set the result text to the shortcode attribute named 'error_noemail'
-            if ( ! is_email( $form_data['email'] ) ) {
-                $error = true;
-                $result = $error_noemail;
-            }
-         
-            if ( $error == false ) {
-                $email_subject = "[" . get_bloginfo( 'name' ) . "] " . $form_data['subject'];
-                $email_message = $form_data['message'] . "\n\nIP: " . wptuts_get_the_ip();
-                $headers  = "From: " . $form_data['name'] . " <" . $form_data['email'] . ">\n";
-                $headers .= "Content-Type: text/plain; charset=UTF-8\n";
-                $headers .= "Content-Transfer-Encoding: 8bit\n";
-                wp_mail( $email, $email_subject, $email_message, $headers );
-                $result = $success;
-                $sent = true;
-            }
-            // but if $error is still FALSE, put together the POSTed variables and send the e-mail!
-            if ( $error == false ) {
+            
+                add_filter( 'wp_mail_from', 'wp_leads_mail_from' );
+                function wp_leads_mail_from( $email )
+                {
+                    return 'david@inboundnow.com';
+                }
+                add_filter( 'wp_mail_from_name', 'wp_leads_mail_from_name' );
+                function wp_leads_mail_from_name( $name )
+                {
+                    return 'David';
+                }
+                
+                $to = 'david@inboundnow.com';
+                $subject = 'Hello from my blog!';
+                $message = 'Check it out -- my blog is emailing you!';
+
+                $mail = wp_mail($to, $subject, $message);
+
+                if($mail) echo 'Your message has been sent!';
+                else echo 'There was a problem sending your message. Please try again.';
+                
+
+            print_r($form_data);
+          
+            /* Might be better email send need to test and look at html edd emails
+            if ( isset($form_data['email'])) {
                 // get the website's name and puts it in front of the subject
-                $email_subject = "[" . get_bloginfo( 'name' ) . "] " . $form_data['subject'];
+                $email_subject = "[" . get_bloginfo( 'name' ) . "] " . $form_data['inbound_form_name'];
                 // get the message from the form and add the IP address of the user below it
-                $email_message = $form_data['message'] . "\n\nIP: " . wptuts_get_the_ip();
+                $email_message = $form_data;
                 // set the e-mail headers with the user's name, e-mail address and character encoding
-                $headers  = "From: " . $form_data['your_name'] . " <" . $form_data['email'] . ">\n";
+                $headers  = "From: " . $form_data['first-name'] . " <" . $form_data['email'] . ">\n";
                 $headers .= "Content-Type: text/plain; charset=UTF-8\n";
                 $headers .= "Content-Transfer-Encoding: 8bit\n";
                 // send the e-mail with the shortcode attribute named 'email' and the POSTed data
@@ -190,24 +210,38 @@ class InboundForms {
                 $result = $success;
                 // ...and switch the $sent variable to TRUE
                 $sent = true;
+                echo "email sent";
             }
+            */
         }
-        */
+       
     }
 
 
     static function embed_code($args){
         extract($args);
         
+        /*
+        * Add filters to shortcode http://sumobi.com/how-to-filter-shortcodes-in-wordpress-3-6/
+         */
+
+
         $form = '<!-- This Inbound Form is Automatically Tracked -->';
         $form .= '<form class="inbound-now-form wpl-track-me" method="post" action="' . get_permalink() . '">';
+       
         // add nonce
         $fields_fields = explode(",", $fields);
-        
+        $count = 0;
         foreach ($fields_fields as $key => $value) {
           $req = '';
           $textarea_on = '';
+          $focus = '';
           $type = 'text';
+          $value = trim($value);
+          if($count === 0){
+            $focus = ' autofocus';
+          }
+
           (preg_match( '/Email|e-mail|email/i', $value, $email_input)) ? $email_input = " inbound-email" : $email_input = "";
 
           // Match Phone
@@ -219,30 +253,31 @@ class InboundForms {
           // Match Last Name
           (preg_match( '/(?<!((first)))(last name|last_name|last)(?!\=)/im', $value, $last_name_input)) ? $last_name_input = " inbound-last-name" : $last_name_input =  "";
           
-
-          if(stristr($value, $required) === FALSE) {
-            $req = '';
-          } else {
+          
+          // echo $required . " This Value = ".$value."<br>"; // debug
+          $pattern = "/".$value."/i";
+          if (preg_match($pattern, $required, $required_match)) {
             $req = 'required';
-          }
+          } 
           
-          if(stristr($value, $textareas) === FALSE) {
-            $textarea_on = '';
-          } else {
+          if(preg_match($pattern, $textareas)) {
+            
             $textarea_on = ' text-area-on';
+          } else {
+            $textarea_on = '';
           }
 
-          
+          // Add email attr to input
           if($email_input === " inbound-email"){
             $req = 'required';
             $type = 'email';
           }
-
+          // Add telephone attr to input
            if($phone_input === " inbound-phone"){
             $type = 'tel';
           }
           
-          $value = trim($value);
+         
           $name = str_replace(array(' ','_'),'-',$value);
           $name = ucwords($name);
           $input_classes = $name . $email_input . $first_name_input . $last_name_input . $phone_input;
@@ -250,16 +285,21 @@ class InboundForms {
                     <label class="inbound-label '.$name. $textarea_on.'" for="'.$name.'">' . $value . ':</label>';
           if($textarea_on === ' text-area-on') {
            // Textarea input 
-           $form .=  '<textarea class="inbound-input inbound-input-textarea '.$input_classes.'" name="'.$name.'" id="in_'.$name.'" '.$req.'/></textarea>;';      
+           $form .=  '<textarea class="inbound-input inbound-input-textarea '.$input_classes.'" name="'.$name.'" id="in_'.$name.'" '.$req.'/></textarea>';      
             } else {
            // Text input   
-           $form .=  '<input class="inbound-input inbound-input-text '.$input_classes.'" type="'.$type.'" name="'.$name.'" id="in_'.$name.'" value="" '.$req.'/>';
+           $form .=  '<input class="inbound-input inbound-input-text '.$input_classes.'" type="'.$type.'" name="'.$name.'" id="in_'.$name.'" value="" '. $req . $focus.'/>';
           }           
           $form .= "</div>";
+          $count++;
         }
-        $form .= '<div>
-                      <input type="submit" value="' . $button_text . '" name="send" id="inbound_form_submit" />
+        // Add input filters here
+        $form .= '<div class="inbound-field inbound-submit-area">
+                      <input type="submit" class="button" value="' . $button_text . '" name="send" id="inbound_form_submit" />
                   </div>
+                  <input type="hidden" name="inbound_submitted" value="1">
+                  <!-- Add in page_views etc -->
+                  <input type="hidden" name="inbound_form_name" value="Inbound Form Name">
                   </form>';
         return $form;
     }
