9f6c77b1493c5376d14d1caab7f6ceb26cea1e8c
Inbound Now <david@inboundnow.com>
2013-11-15 20:36:36 -0800

    adding version # checks to plugin activation

--- wordpress-cta.php
+++ wordpress-cta.php
@@ -19,9 +19,9 @@ define('WP_CTA_UPLOADS_PATH', $uploads['basedir'].'/wp-calls-to-action/templates
 define('WP_CTA_UPLOADS_URLPATH', $uploads['baseurl'].'/wp-calls-to-action/templates/' );
 
 
+
 /* Inbound Core Shared Files. */
 add_action( 'plugins_loaded', 'inbound_load_shared' );
-
 
 function inbound_load_shared(){
 	include_once('shared/tracking/store.lead.php'); // Lead Storage from cta
@@ -92,10 +92,29 @@ if (is_admin())
  * REGISTER ACTIVATION HOOK
  */
 
-register_activation_hook(__FILE__, 'wp_call_to_action_activate');
-function wp_call_to_action_activate()
-{
 
+
+register_activation_hook(__FILE__, 'wp_call_to_action_activate');
+function wp_call_to_action_activate($wp = '3.9', $php = '5.2.4', $lp = '1.3.1', $leads = '1.1.1')
+{
+	global $wp_version;
+	if ( version_compare( PHP_VERSION, $php, '<' ) ) {
+	    $flag = 'PHP';
+	    $version = 'PHP' == $flag ? $php : $wp;
+		wp_die('<p>The <strong>WordPress Calls to Action</strong> plugin requires'.$flag.'  version '.$php.' or greater.</p>','Plugin Activation Error',  array( 'response'=>200, 'back_link'=>TRUE ) );
+		deactivate_plugins( basename( __FILE__ ) );
+	} elseif ( version_compare( $wp_version, $wp, '<' ) ) {
+	    $flag = 'WordPress';
+	    wp_die('<p>The <strong>WordPress Calls to Action</strong> plugin requires'.$flag.'  version '.$wp.' or greater.</p>','Plugin Activation Error',  array( 'response'=>200, 'back_link'=>TRUE ) );
+	    deactivate_plugins( basename( __FILE__ ) );
+	} elseif (defined('LANDINGPAGES_CURRENT_VERSION') && version_compare( LANDINGPAGES_CURRENT_VERSION, $lp, '<' )){
+		$flag = 'Landing Pages';
+		wp_die('<p>The <strong>WordPress Calls to Action</strong> plugin requires '.$flag.'  version '.$lp.' or greater. <br><br>Please Update WordPress Landing Page Plugin to update Calls to action</p>','Plugin Activation Error',  array( 'response'=>200, 'back_link'=>TRUE ) );
+	} elseif (defined('LEADS_CURRENT_VERSION') && version_compare( LEADS_CURRENT_VERSION, $leads, '<' )){
+		$flag = 'Leads';
+		wp_die('<p>The <strong>WordPress Calls to Action</strong> plugin requires '.$flag.'  version '.$leads.' or greater. <br><br>Please Update WordPress Leads Plugin to update Calls to action</p>','Plugin Activation Error',  array( 'response'=>200, 'back_link'=>TRUE ) );
+	} else {
+	// Activate Plugin
 	add_option( 'wp_cta_global_css', '', '', 'no' );
 	add_option( 'wp_cta_global_js', '', '', 'no' );
 	add_option( 'wp_cta_global_record_admin_actions', '1', '', 'no' );
@@ -104,7 +123,7 @@ function wp_call_to_action_activate()
 
 	global $wp_rewrite;
 	$wp_rewrite->flush_rules();
-
+	}
 	// Add default CTA setup and setup 3 categores: sidebar, blog post, popup
 
 }
