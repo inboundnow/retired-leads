07a2001f6286dec15d8ad90aa0dd62fc6a4c04d3
Hudson Atwell <hudson.atwell@gmail.com>
2014-01-29 21:27:44 -0600

    Updating shared global settings to add master license key to cta and leads

--- shared/inboundnow/inboundnow.global-settings.php
+++ shared/inboundnow/inboundnow.global-settings.php
@@ -5,28 +5,42 @@ if (!function_exists('inboundnow_add_master_license'))
 	/* Add Master License Key Setting*/
 	add_filter('lp_define_global_settings', 'inboundnow_add_master_license', 1, 1);
 	add_filter('wpleads_define_global_settings', 'inboundnow_add_master_license', 1, 1);
-	add_filter('wpcta_define_global_settings', 'inboundnow_add_master_license', 1, 1);
-	function inboundnow_add_master_license($lp_global_settings)
+	add_filter('wp_cta_define_global_settings', 'inboundnow_add_master_license', 1, 1);
+	function inboundnow_add_master_license($global_settings)
 	{
-		if (array_key_exists('lp-license-keys',$lp_global_settings))
+		$key = '';
+		switch(current_filter())
 		{
-			$lp_global_settings['lp-license-keys']['settings'][] = 	array(
-					'id'  => 'extensions-license-keys-master-key-header',
-					'description' => __( "Head to http://www.inboundnow.com/ to retrieve your extension-ready license key." , LANDINGPAGES_TEXT_DOMAIN),
-					'type'  => 'header',
-					'default' => '<h3 class="lp_global_settings_header">'. __( 'InboundNow Master Key' , LANDINGPAGES_TEXT_DOMAIN) .'</h3>'
-			);
-			
-			$lp_global_settings['lp-license-keys']['settings'][] = 	array(
-					'id'  => 'inboundnow_master_license_key',
-					'option_name'  => 'inboundnow_master_license_key',
-					'label' => __('InboundNow Master License Key' , LANDINGPAGES_TEXT_DOMAIN),
-					'description' => __( "Head to http://www.inboundnow.com/ to retrieve your extension-ready license key." , LANDINGPAGES_TEXT_DOMAIN),
-					'type'  => 'text',
-					'default' => ''
-			);
+			case 'lp_define_global_settings':
+				$key = 'lp-license-keys';
+				$text_domain = LANDINGPAGES_TEXT_DOMAIN;
+				break;
+			case 'wpleads_define_global_settings':
+				$key = 'wpleads-license-keys';
+				$text_domain = WPL_TEXT_DOMAIN;
+				break;
+			case 'wp_cta_define_global_settings':
+				$key = 'wp-cta-license-keys';
+				$text_domain = WP_CTA_TEXT_DOMAIN;
+				break;	
 		}
-
-		return $lp_global_settings;
+		
+		$global_settings[$key]['settings'][] = 	array(
+						'id'  => 'extensions-license-keys-master-key-header',
+						'description' => __( "Head to http://www.inboundnow.com/ to retrieve your extension-ready license key." , $text_domain ),
+						'type'  => 'header',
+						'default' => '<h3 class="lp_global_settings_header">'. __( 'InboundNow Master Key' , $text_domain ) .'</h3>'
+				);
+				
+		$global_settings[$key]['settings'][] = 	array(
+				'id'  => 'inboundnow_master_license_key',
+				'option_name'  => 'inboundnow_master_license_key',
+				'label' => __('InboundNow Master License Key' , $text_domain ),
+				'description' => __( "Head to http://www.inboundnow.com/ to retrieve your extension-ready license key." , $text_domain ),
+				'type'  => 'text',
+				'default' => ''
+		);
+		
+		return $global_settings;
 	}
 }
