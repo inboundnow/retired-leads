dfc8d48f0a50159fcc4f83bbf84a95e7c9c8c697
Inbound Now <david@inboundnow.com>
2014-02-17 18:26:19 -0800

    updating to new field mapping

--- shared/assets/frontend/js/store.lead.ajax.js
+++ shared/assets/frontend/js/store.lead.ajax.js
@@ -1,10 +1,134 @@
+var inbound_form_data = inbound_form_data || {};
+
+function add_inbound_form_class(el, value) {
+  var value = value.replace(" ", "_");
+  var value = value.replace("-", "_");
+  el.addClass('inbound_map_value');
+  el.attr('data-inbound-form-map', 'inbound_map_' + value);
+}
+
+function get_inbound_form_value(el) {
+  var value = el.value;
+  return value;
+}
+
+// Build Form Object
+function inbound_map_fields(el, value, Obj) {
+  var formObj = [];
+  var $this = el;
+  var clean_output = value;
+  var label = $this.closest('label').text();
+  var exclude = ['credit-card']; // exlcude values from formObj
+  var inarray = jQuery.inArray(clean_output, exclude);
+  if(inarray == 0){
+  	return null;
+  }
+  // Add items to formObj
+  formObj.push({
+  				field_label: label,
+                field_name: $this.attr("name"),
+                field_value: $this.attr("value"),
+                field_id: $this.attr("id"),
+                field_class: $this.attr("class"),
+                field_type: $this.attr("type"),
+                match: clean_output,
+                js_selector: $this.attr("data-js-selector")
+              });
+  return formObj;
+}
+
+// Trim Whitespace
+function trim(s) {
+    s = s.replace(/(^\s*)|(\s*$)/gi,"");
+    s = s.replace(/[ ]{2,}/gi," ");
+    s = s.replace(/\n /,"\n"); return s;
+}
+
+// Run Form Mapper
+function run_field_map_function(el, lookingfor) {
+  var return_form;
+  var formObj = new Array();
+  var $this = el;
+
+  var this_val = $this.attr("value");
+      var array = lookingfor.split(",");
+      var array_length = array.length - 1;
+
+      // Main Loop
+      for (var i = 0; i < array.length; i++) {
+          var clean_output = trim(array[i]);
+          var nice_name = clean_output.replace(/^\s+|\s+$/g,'');
+          var nice_name = nice_name.replace(" ",'-');
+          //console.log(clean_output);
+
+          // Look for attr name match
+          if ($this.attr("name").toLowerCase().indexOf(clean_output)>-1) {
+            var the_map = inbound_map_fields($this, clean_output, formObj);
+            add_inbound_form_class($this, clean_output);
+            console.log('match name: ' + clean_output);
+            inbound_form_data[nice_name] = this_val;
+          }
+          // look for id match
+          else if ($this.attr("id").toLowerCase().indexOf(clean_output)>-1) {
+            var the_map = inbound_map_fields($this, clean_output, formObj);
+            add_inbound_form_class($this, clean_output);
+            console.log('match id: ' + clean_output);
+            inbound_form_data[nice_name] = this_val;
+          }
+          // Look for label name match
+          else if ($this.closest('li').children('label').length>0)
+          {
+            if ($this.closest('li').children('label').html().toLowerCase().indexOf(clean_output)>-1)
+            {
+              var the_map = inbound_map_fields($this, clean_output, formObj);
+              add_inbound_form_class($this, clean_output);
+              console.log('match li: ' + clean_output);
+              inbound_form_data[nice_name] = this_val;
+            }
+          }
+          // Look for closest div label name match
+          else if ($this.closest('div').children('label').length>0)
+          {
+            if ($this.closest('div').children('label').html().toLowerCase().indexOf(clean_output)>-1)
+            {
+              var the_map = inbound_map_fields($this, clean_output, formObj);
+              add_inbound_form_class($this, clean_output);
+              console.log('match div: ' + clean_output);
+              inbound_form_data[nice_name] = this_val;
+            }
+          } else {
+          	return false;
+          }
+      }
+      return_form = the_map;
+
+  return inbound_form_data;
+}
+
+function return_mapped_values(this_form) {
+	// Map form fields
+	jQuery(this_form).find('input[type=text],input[type=email],textarea,select').each(function() {
+		var this_input = jQuery(this);
+		if (this.value) {
+		var inbound_form_data = run_field_map_function( this_input, "job title, first name, last name, email, e-mail, company, phone, tele, address, name");
+		}
+		return inbound_form_data;
+	});
+	return inbound_form_data;
+}
+
+function merge_form_options(obj1,obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
+
 jQuery(document).ready(function($) {
 /* Core Inbound Form Tracking Script */
-
 	jQuery("body").on('submit', '.wpl-track-me', function (e) {
 
 		this_form = jQuery(this);
-
 		element_type = 'FORM';
 
 		// process form only once
@@ -13,8 +137,8 @@ jQuery(document).ready(function($) {
 			return;
 		}
 
-		form_id = jQuery(this).attr('id');
-		form_class = jQuery(this).attr('class');
+		form_id = this_form.attr('id');
+		form_class = this_form.attr('class');
 
 		e.preventDefault(); // halt normal form
 
@@ -31,115 +155,39 @@ jQuery(document).ready(function($) {
 		jQuery('input').css('cursor', 'wait');
 		jQuery('body').css('cursor', 'wait');
 
-		var email = "";
-		var firstname = "";
-		var lastname = "";
-		var phone = "";
-		var company = "";
-		var address = "";
 
+		/* Define Variables */
 		var tracking_obj = "";
 		var page_view_count = countProperties(pageviewObj);
-		//console.log("view count" + page_view_count);
+		var inbound_form_data = inbound_form_data || {}; // Dynamic JS object for passing custom values. This can be hooked into by third parties by using the below syntax.
+		inbound_form_data['leads_list'] = jQuery(this_form).find('#inbound_form_lists').val();
+		var source = jQuery.cookie("wp_lead_referral_site");
 
-		if (!email)
-		{
+		// Map form fields
+		jQuery(this_form).find('input[type=text],input[type=email],textarea,select').each(function() {
+			var this_input = jQuery(this);
+			if (this.value) {
+			var inbound_form_data = run_field_map_function( this_input, "job title, first name, last name, email, e-mail, company, phone, tele, address");
+			}
+		});
 
-			 jQuery(this_form).find('input[type=text],input[type=email]').each(function() {
-				if (this.value)
-				{
-					if (jQuery(this).attr("name").toLowerCase().indexOf('email')>-1&&!email) {
-						email = this.value;
+		var returned_form_data = return_mapped_values(this_form);
+		var inbound_form_data = merge_form_options(inbound_form_data,returned_form_data);
+		console.log(inbound_form_data);
 
-					}
-					else if(jQuery(this).attr("name").toLowerCase().indexOf('e-mail')>-1&&!email) {
-						 email = this.value;
-					}
-					else if(jQuery(this).attr("name").toLowerCase().indexOf('name')>-1&&!firstname) {
-						 firstname = this.value;
-					}
-					else if (jQuery(this).attr("name").toLowerCase().indexOf('last')>-1) {
-						 lastname = this.value;
-					}
-					else if (jQuery(this).attr("name").toLowerCase().indexOf('phone')>-1) {
-						 phone = this.value;
-					}
-					else if (jQuery(this).attr("name").toLowerCase().indexOf('company')>-1) {
-						 company = this.value;
-					}
-					else if (jQuery(this).attr("name").toLowerCase().indexOf('address')>-1) {
-						 address = this.value;
-					}
-				}
-			});
+		// Set variables
+		var email = inbound_form_data['email'] || false; // back fallback
+		var firstname = inbound_form_data['first-name'] || false;
+		var lastname = inbound_form_data['last-name'] || "";
+		var phone = inbound_form_data['phone'] || "";
+		var company = inbound_form_data['company'] || "";
+		var address = inbound_form_data['address'] || "";
+
+		if(!firstname){
+		  var firstname = inbound_form_data['name'] || false;
 		}
 
-		if (!email)
-		{
-			jQuery(this_form).find('input[type=text],input[type=email]').each(function() {
-				if (jQuery(this).closest('li').children('label').length>0)
-				{
-					if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('email')>-1&&!email)
-					{
-						email = this.value;
-					}
-					else if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('e-mail')>-1&&!email) {
-						email =  this.value;
-					}
-					else if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('name')>-1&&!firstname) {
-						firstname = this.value;
-					}
-					else if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('last')>-1) {
-						lastname = this.value;
-					}
-					else if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('phone')>-1) {
-						 phone = this.value;
-					}
-					else if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('company')>-1) {
-						 company = this.value;
-					}
-					else if (jQuery(this).closest('li').children('label').html().toLowerCase().indexOf('address')>-1) {
-						 address = this.value;
-					}
-				}
-
-			});
-		}
-
-		if (!email)
-		{
-			jQuery(this_form).find('input[type=text],input[type=email]').each(function() {
-				if (jQuery(this).closest('div').children('label').length>0)
-				{
-					if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('email')>-1&&!email)
-					{
-						email = this.value;
-					}
-					else if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('e-mail')>-1&&!email) {
-						email = this.value;
-					}
-					else if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('name')>-1&&!firstname) {
-						firstname = this.value;
-					}
-					else if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('last')>-1) {
-						lastname = this.value;
-					}
-					else if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('phone')>-1) {
-						 phone = this.value;
-					}
-					else if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('company')>-1) {
-						 company = this.value;
-					}
-					else if (jQuery(this).closest('div').children('label').html().toLowerCase().indexOf('address')>-1) {
-						 address = this.value;
-					}
-				}
-			});
-		}
-
-
-		if (!lastname&&firstname)
-		{
+		if (!lastname&&firstname) {
 			var parts = firstname.split(" ");
 			firstname = parts[0];
 			lastname = parts[1];
@@ -148,9 +196,19 @@ jQuery(document).ready(function($) {
 		var form_inputs = jQuery('.wpl-track-me').find('input[type=text],input[type=hidden],textarea,select');
 		var post_values = {};
 		// unset values with exclude array
+		var inbound_exclude = inbound_exclude || [];
+		inbound_exclude.push('inbound_furl', 'inbound_current_page_url', 'inbound_notify', 'inbound_submitted', 'post_type', 'post_status', 's');
+
 		form_inputs.each(function() {
-			post_values[this.name] = jQuery(this).val();
+			if (jQuery.inArray(this.name, inbound_exclude) === -1){
+			   post_values[this.name] = jQuery(this).val();
+			}
+			if (this.value.indexOf('@')>-1&&!email){
+				email = jQuery(this).val();
+				inbound_form_data['email'] = email;
+			}
 		});
+
 		var post_values_json = JSON.stringify(post_values);
 
 		var wp_lead_uid = jQuery.cookie("wp_lead_uid");
@@ -166,15 +224,6 @@ jQuery(document).ready(function($) {
 
 		jQuery.cookie("wp_lead_email", email, { path: '/', expires: 365 });
 
-
-		var source = jQuery.cookie("wp_lead_referral_site");
-		/* Timeout Fallback
-		setTimeout(function() {
-			console.log('more than 10 seconds has passed. Release form')
-			release_form_sub();
-		}, 10000);
-		*/
-
 		// Ensure global _gaq Google Analytics queue has been initialized.
 		 var _gaq = _gaq || [];
 
@@ -185,6 +234,7 @@ jQuery(document).ready(function($) {
 		var lp_check = (inbound_ajax.post_type === 'landing-page') ? 'Landing Page' : "";
 		var cta_check = (inbound_ajax.post_type === 'wp-call-to-action') ? 'Call to Action' : "";
 		var page_type = (!cta_check && !lp_check) ? inbound_ajax.post_type : lp_check + cta_check;
+		var post_form_data = JSON.stringify(inbound_form_data);
 
 		jQuery.ajax({
 			type: 'POST',
@@ -207,7 +257,8 @@ jQuery(document).ready(function($) {
 				json: tracking_obj, // replace with page_view_obj
 				raw_post_values_json : post_values_json,
 				lp_id: page_id,
-				source: source
+				source: source,
+				Form_Data: post_form_data
 				/* Replace with jquery hook
 					do_action('wpl-lead-collection-add-ajax-data');
 				*/
@@ -377,180 +428,71 @@ jQuery(document).ready(function($) {
 
 
 
-	});
+});
 
 
-	// Fallback for form ajax fails
-	var failed_conversion = jQuery.cookie("failed_conversion");
-	var fallback_obj = jQuery.totalStorage('failed_conversion');
+/*  Fallback for form ajax fails */
+var failed_conversion = jQuery.cookie("failed_conversion");
+var fallback_obj = jQuery.totalStorage('failed_conversion');
 
-	if (typeof (failed_conversion) != "undefined" && failed_conversion == 'true' ) {
-		if (typeof fallback_obj =='object' && fallback_obj)
-		{
-			//console.log('fallback ran');
-				jQuery.ajax({
-					type: 'POST',
-					url: inbound_ajax.admin_url,
-					data: {
-							action: fallback_obj[0].action,
-							emailTo: fallback_obj[0].emailTo,
-							first_name: fallback_obj[0].first_name,
-							last_name: fallback_obj[0].last_name,
-							wp_lead_uid: fallback_obj[0].wp_lead_uid,
-							page_view_count: fallback_obj[0].page_view_count,
-							page_views: fallback_obj[0].page_views,
-							post_type: fallback_obj[0].post_type,
-							lp_variation: fallback_obj[0].lp_variation,
-							json: fallback_obj[0].json, // replace with page_view_obj
-							// type: 'form-completion',
-							raw_post_values_json : fallback_obj[0].raw_post_values_json,
-							lp_id: fallback_obj[0].lp_id
-							/* Replace with jquery hook
-								do_action('wpl-lead-collection-add-ajax-data');
-							*/
-						},
-					success: function(user_id){
-						//console.log('Fallback fired');
-						jQuery.removeCookie("failed_conversion"); // remove failed cookie
-						jQuery.totalStorage.deleteItem('failed_conversion'); // remove failed data
-						jQuery.totalStorage.deleteItem('cpath'); // remove cpath
-						   },
-					error: function(MLHttpRequest, textStatus, errorThrown){
-							//alert(MLHttpRequest+' '+errorThrown+' '+textStatus);
-							//die();
-						}
+if (typeof (failed_conversion) != "undefined" && failed_conversion == 'true' ) {
+	if (typeof fallback_obj =='object' && fallback_obj) {
+		//console.log('fallback ran');
+			jQuery.ajax({
+				type: 'POST',
+				url: inbound_ajax.admin_url,
+				data: {
+						action: fallback_obj[0].action,
+						emailTo: fallback_obj[0].emailTo,
+						first_name: fallback_obj[0].first_name,
+						last_name: fallback_obj[0].last_name,
+						wp_lead_uid: fallback_obj[0].wp_lead_uid,
+						page_view_count: fallback_obj[0].page_view_count,
+						page_views: fallback_obj[0].page_views,
+						post_type: fallback_obj[0].post_type,
+						lp_variation: fallback_obj[0].lp_variation,
+						json: fallback_obj[0].json, // replace with page_view_obj
+						// type: 'form-completion',
+						raw_post_values_json : fallback_obj[0].raw_post_values_json,
+						lp_id: fallback_obj[0].lp_id
+						/* Replace with jquery hook
+							do_action('wpl-lead-collection-add-ajax-data');
+						*/
+					},
+				success: function(user_id){
+					//console.log('Fallback fired');
+					jQuery.removeCookie("failed_conversion"); // remove failed cookie
+					jQuery.totalStorage.deleteItem('failed_conversion'); // remove failed data
+					jQuery.totalStorage.deleteItem('cpath'); // remove cpath
+					   },
+				error: function(MLHttpRequest, textStatus, errorThrown){
+						//alert(MLHttpRequest+' '+errorThrown+' '+textStatus);
+						//die();
+					}
 
-				});
-		}
+			});
 	}
-
- });
-
-/* GA functions */
-/* _trackEvent(category, action, opt_label, opt_value, opt_noninteraction)
-
-<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" onclick="log_event('Newsletter', 'Subscribe', 'Footer Newsletter Subscribe');" class="button">
-
-// Ensure global _gaq Google Analytics queue has been initialized.
- var _gaq = _gaq || [];
-
-function log_event(category, action, label) {
-  _gaq.push(['_trackEvent', category, action, label]);
 }
 
-log_event('Newsletter', 'Subscribe', 'Footer Newsletter Subscribe');
-
-function log_click(category, link) {
-  log_event(category, 'Click', $(link).text());
-}
-
-$(document).ready(function() {
-  $('nav a').click(function() {
-    log_click('Navigation', this);
-  });
-})
-
-*/
+});
 
 function release_form_sub(this_form , element_type){
 	jQuery('button, input[type="button"]').css('cursor', 'default');
 	jQuery('input').css('cursor', 'default');
 	jQuery('body').css('cursor', 'default');
 
-	if (element_type=='FORM')
-	{
+	if (element_type=='FORM') {
 		this_form.unbind('submit');
 		this_form.submit();
 	}
 
-	if (element_type=='A')
-	{
+	if (element_type=='A') {
 		this_form.unbind('wpl-track-me');
 
-		if (a_href)
-		{
+		if (a_href) {
 			window.location = a_href;
-		}
-		else
-		{
+		} else {
 			location.reload();
 		}
 	}
 }
-
- function inbound_find_form_fields(element, field_name, regex) {
-	//console.log(element);
-	//console.log(field_name);
-	var return_val = "";
-	var name = element.attr("name");
-	var id = element.attr("id");
-	var form_value = element.val();
-	var nearest_li = element.closest('li').children('label');
-	var nearest_div = element.closest('div').children('label');
-	var newregex = new RegExp(regex, 'gi');
-	//console.log(newregex);
-
-	// Check name attributes for common names
-	if (typeof (name) != "undefined" && name != null && name != "" && return_val === "") {
-
-		var match = newregex.test(name); // regex to find matching name
-		//console.log(match + name);
-		if (match == true) {
-			return form_value + " Regex 'name' Match: " + name;
-			var return_val = form_value;
-		}
-		if (name.toLowerCase().indexOf(field_name)>-1) {
-			return form_value + " indexof Match: " + name;
-			var return_val = form_value;
-		}
-
-	}
-
-	// Check nearest li element for common names
-	if (typeof (nearest_li) != "undefined" && nearest_li != null && nearest_li != "" && return_val === "") {
-		var the_label_text = nearest_li.html();
-		var match = newregex.test(the_label_text); // regex to find matching label
-
-		if (match == true){
-			return the_label_text + " Regex Label Match" + name;
-			var return_val = form_value;
-		}
-
-	}
-
-	// Check nearest div element for common names
-	if (typeof (nearest_div) != "undefined" && nearest_div != null && nearest_div != "" && return_val === "") {
-		var the_div_text = nearest_div.html();
-		var match = newregex.test(the_div_text); // regex to find matching label
-
-		if (match == true){
-			return the_div_text + " Regex Div Match" + name;
-			var return_val = form_value;
-		}
-
-	}
-
-	if(return_val === "") {
-		//return "Not Found:" + name + "Looking for:" + field_name;
-		return;
-	}
-
-}
-
-// Regex to match form field values
-/* Runs the above function and grabs form values
-setTimeout(function() {
-jQuery(".wpl-track-me").find('input[type=text],input[type=email]').each(function() {
-			var this_input = jQuery(this);
-
-			var email_field = inbound_find_form_fields(this_input, 'email', 'email|e-mail');
-			var first_name_field = inbound_find_form_fields(this_input, 'first', 'first name|first-name|first_name');
-			var last_name_field =  inbound_find_form_fields(this_input, 'last', 'Last name|last-name|last_name');
-
-			console.log(email_field);
-			console.log(first_name_field);
-			console.log(last_name_field);
-});
-}, 400);
-*/
-	// end function to parse form fields
