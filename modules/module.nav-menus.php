10822bc611e38eb36e5895be73985f99d83a5d99
Inbound Now <david@inboundnow.com>
2014-03-18 13:28:40 -0700

    syntax updates

--- modules/module.nav-menus.php
+++ modules/module.nav-menus.php
@@ -3,10 +3,8 @@
 /**********************************************************/
 /******************CREATE SETTINGS SUBMENU*****************/
 add_action('admin_menu', 'wpleads_add_menu');
-function wpleads_add_menu()
-{
-	if (current_user_can('manage_options'))
-	{
+function wpleads_add_menu() {
+	if (current_user_can('manage_options')) {
 		add_submenu_page('edit.php?post_type=wp-lead', __( 'Lead Management' , 'leads' ) , __( 'Lead Management' , 'leads' ), 'manage_options', 'lead_management','lead_management_admin_screen');
 
 		//add_submenu_page('edit.php?post_type=wp-lead', 'Lead Rules', 'Lead Rules', 'manage_options', 'lead-rules-redirect',100);
@@ -18,7 +16,6 @@ function wpleads_add_menu()
 
 	/* remove main lead menu from nav*/
     global $submenu;
-
     unset($submenu['post-new.php?post_type=wp-lead'][15]);
 	remove_submenu_page('edit.php?post_type=wp-lead', 'post-new.php?post_type=wp-lead');
 
--- modules/module.post-type.list.php
+++ modules/module.post-type.list.php
@@ -24,26 +24,19 @@ function wpleads_count_associated_lead_items($post_id){
 
 
 
-function wpleads_add_list_to_wplead_list_category_taxonomy($post_id, $list_title, $list_slug = null)
-{
+function wpleads_add_list_to_wplead_list_category_taxonomy($post_id, $list_title, $list_slug = null) {
 
 	$wplead_cat_id = get_post_meta( $post_id, 'wplead_list_category_id', true);
-	if ($wplead_cat_id)
-	{
+	if ($wplead_cat_id) {
 		wp_update_term( $wplead_cat_id, 'wplead_list_category', array('name'=>$list_title) );
-	}
-	else
-	{
+	} else {
 		//add category taxonomy in wpleads
 		$term = wp_insert_term( $list_title, 'wplead_list_category', $args = array('slug'=>$list_slug) );
 
-		if (is_object($term))
-		{
+		if (is_object($term)) {
 			$term_error_array = $term->error_data;
 			$term_id = $term_error_array['term_exists'];
-		}
-		else
-		{
+		} else {
 			$term_id = $term['term_id'];
 		}
 
@@ -51,8 +44,7 @@ function wpleads_add_list_to_wplead_list_category_taxonomy($post_id, $list_title
 	}
 }
 
-function wpleads_add_lead_to_list( $list_id, $lead_id )
-{
+function wpleads_add_lead_to_list( $list_id, $lead_id ) {
 
 	$wplead_cat_id = get_post_meta($list_id,'wplead_list_category_id', true);
 
@@ -61,8 +53,7 @@ function wpleads_add_lead_to_list( $list_id, $lead_id )
 	//build meta pair for list ids lead belongs to
 	$wpleads_list_ids = get_post_meta($lead_id, 'wpleads_list_ids', true);
 
-	if ($wpleads_list_ids)
-	{
+	if ($wpleads_list_ids) {
 		$wpleads_list_ids_new = array();
 
 		//get array
@@ -74,10 +65,8 @@ function wpleads_add_lead_to_list( $list_id, $lead_id )
 		delete_post_meta($lead_id, 'wpleads_list_ids');
 
 		//restore
-		foreach ($wpleads_list_ids as $key=>$value)
-		{
-			if ($value)
-			{
+		foreach ($wpleads_list_ids as $key=>$value) {
+			if ($value) {
 				//echo $value;
 				$list = get_post($value['list_id']);
 				$list_name = $list->post_name;
@@ -89,8 +78,7 @@ function wpleads_add_lead_to_list( $list_id, $lead_id )
 		}
 
 		//push newest if not exists
-		if (!in_array($list_id, $wpleads_list_ids_new))
-		{
+		if (!in_array($list_id, $wpleads_list_ids_new)) {
 			$list = get_post($list_id);
 			$list_name = $list->post_name;
 
@@ -105,9 +93,7 @@ function wpleads_add_lead_to_list( $list_id, $lead_id )
 
 		$wpleads_list_ids_new = json_encode($wpleads_list_ids_new);
 		$wpleads_list_ids_new = update_post_meta($lead_id, 'wpleads_list_ids', $wpleads_list_ids_new);
-	}
-	else
-	{
+	} else {
 		// REWRITE TO GET TAX ID
 		$list = get_post($list_id);
 		$list_name = $list->post_name;
@@ -120,8 +106,7 @@ function wpleads_add_lead_to_list( $list_id, $lead_id )
 
 }
 
-function wpleads_remove_lead_from_list( $list_id, $lead_id )
-{
+function wpleads_remove_lead_from_list( $list_id, $lead_id ) {
 	$categories = wp_get_post_terms( $lead_id, 'wplead_list_category', array( 'fields'=>'ids' ) );
 
 	$list_category_id = get_post_meta($list_id,'wplead_list_category_id', true);
@@ -137,24 +122,22 @@ function wpleads_remove_lead_from_list( $list_id, $lead_id )
 	//build meta pair for list ids lead belongs to
 	$wpleads_list_ids = get_post_meta($lead_id, 'wpleads_list_ids' , true);
 
-	if ($wpleads_list_ids)
-	{
+	if ($wpleads_list_ids) {
 		//get array
 		$wpleads_list_ids = json_decode($wpleads_list_ids, true);
 
-		if ( !is_array(wpleads_list_ids) )
+		if ( !is_array($wpleads_list_ids)) {
 			$wpleads_list_ids = array();
+		}
 
 		//clean
 		delete_post_meta($lead_id, 'wpleads_list_ids');
 
 		//rebuild
-		foreach ($wpleads_list_ids as $key=>$value)
-		{
+		foreach ($wpleads_list_ids as $key=>$value) {
 			if ($value['ID']==$list_id)
 				unset($wpleads_list_ids[$key]);
 		}
-
 		//store
 		$wpleads_list_ids = json_encode($wpleads_list_ids);
 		$wpleads_list_ids = update_post_meta($lead_id, 'wpleads_list_ids', $wpleads_list_ids);
@@ -168,7 +151,7 @@ function wpleads_get_lead_lists_as_array() {
 	$args = array(
 	    'hide_empty' => false,
 	);
-	
+
 	$terms = get_terms('wplead_list_category', $args);
 
 	foreach ( $terms as $term  ) {
