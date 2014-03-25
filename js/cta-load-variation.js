7fb8fe6b4cb81e9b81d7d3c8099d8ffeacd953fc
Hudson Atwell <hudson.atwell@gmail.com>
2014-03-24 21:26:48 -0500

    Adding the ability to disable split testing to save server performance.

--- js/cta-load-variation.js
+++ js/cta-load-variation.js
@@ -1,39 +1,49 @@
-function wp_cta_load_variation(cta_id)
+function wp_cta_load_variation( cta_id , disable_ajax )
 {
 	/* load & reveal variation */
 	//console.log('Variation Ajax URL:' + cta_variation.ajax_url + '?cta_id='+cta_id);
+	if ( parseInt(disable_ajax) != 1 ) {
+		jQuery.ajax({
+			 type: "GET",
+			 url: cta_variation.ajax_url,
+			 dataType: "script",
+			 async:false,
+			 data : {
+			  'cta_id' : cta_id
+			 },
+			 success: function(vid) {
+				var loaded_ctas = {};
+				var loaded_local_cta = jQuery.totalStorage('wp_cta_loaded');
+				if (loaded_local_cta != null) {
+					var loaded_ctas = JSON.parse(localStorage.getItem('wp_cta_loaded'));
+				}
 
-	jQuery.ajax({
-		 type: "GET",
-		 url: cta_variation.ajax_url,
-		 dataType: "script",
-		 async:false,
-		 data : {
-		  'cta_id' : cta_id
-		 },
-		 success: function(vid) {
-		 	var loaded_ctas = {};
-		 	var loaded_local_cta = jQuery.totalStorage('wp_cta_loaded');
-		 	if (loaded_local_cta != null) {
-		 		var loaded_ctas = JSON.parse(localStorage.getItem('wp_cta_loaded'));
-		 	}
+				loaded_ctas[cta_id] = vid;
 
-			loaded_ctas[cta_id] = vid;
-			// localStorage.setItem('wp_cta_loaded', JSON.stringify(loaded_ctas)); // old syntax
-
-			jQuery.totalStorage('wp_cta_loaded', loaded_ctas); // store cta data
-			console.log('WP CTA Load Object Updated:' + JSON.stringify(loaded_ctas));
+				jQuery.totalStorage('wp_cta_loaded', loaded_ctas); // store cta data
+				console.log('WP CTA Load Object Updated:' + JSON.stringify(loaded_ctas));
+			}
+		});
+	} else {
+		var loaded_ctas = {};
+		var loaded_local_cta = jQuery.totalStorage('wp_cta_loaded');
+		if (loaded_local_cta != null) {
+			var loaded_ctas = JSON.parse(localStorage.getItem('wp_cta_loaded'));
 		}
-	});
+
+		loaded_ctas[cta_id] = 0;
+
+		jQuery.totalStorage('wp_cta_loaded', loaded_ctas); // store cta data
+		console.log('WP CTA Load Object Updated:' + JSON.stringify(loaded_ctas));
+	}
 }
 
 jQuery(document).ready(function($) {
 
 	/* reset local storage variable every page load */
-	//localStorage.removeItem('wp_cta_loaded');
 	jQuery.totalStorage.deleteItem('wp_cta_loaded'); // remove pageviews
 
 	if (cta_variation.cta_id) {
-		wp_cta_load_variation(cta_variation.cta_id);
+		wp_cta_load_variation( cta_variation.cta_id , cta_variation.disable_ajax );
 	}
 });
