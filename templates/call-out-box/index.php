50e5d81986ff5a4a182051feb4612f0f450d612d
Hudson Atwell <hudson.atwell@gmail.com>
2014-06-03 15:02:52 -0500

    Fixing CSS issues with call out box. Debugging preview mode.

--- templates/call-out-box/index.php
+++ templates/call-out-box/index.php
@@ -1,54 +1,55 @@
 <style>
 
-.wp_cta_container #content 
+#cta_container #content 
 {
 	background: transparent;
 }
-.wp_cta_container {
+#cta_container {
 	margin:auto; 
 
 }
-.wp_cta_container #content {
+#cta_container #content {
 	width: 400px;
 	background: #222;
 	padding-bottom: 15px;
 }
 
-.wp_cta_container p {
+#cta_container p {
 
-	text-align: center;
+	text-align: {{content-alignment}};
 	color:#fff;
 }
 
-.wp_cta_container p:first-child {
-margin-top: 0px;
-padding-top: 0px;
+#cta_container p:first-child {
+	margin-top: 0px;
+	padding-top: 0px;
 }
-.wp_cta_container p:last-child {
-margin-bottom: 0px;
-padding-bottom: 0px;
+#cta_container p:last-child {
+	margin-bottom: 0px;
+	padding-bottom: 0px;
 }
 
 
-@import url(https://fonts.googleapis.com/css?family=Lato:300,400,600);
-.wp_cta_container {
-  text-align: center;
-  font-family: 'Lato', Calibri, Helvetica, Arial, sans-serif;
+/*@import url(https://fonts.googleapis.com/css?family={{font}}:300,400,600); */ 
+#cta_container {
+  text-align: {{content-alignment}};
+  font-family: Calibri, Helvetica, Arial, sans-serif;
   font-weight: 300;
 }
 
-.wp_cta_container .the_content {
-padding-left: 10px;
-padding-right: 10px;
-padding: 10px;
-display: block;
-width: 80%;
-margin: auto;
+#cta_container .the_content {
+	font-family: Calibri, Helvetica, Arial, sans-serif;
+	padding-left: 10px;
+	padding-right: 10px;
+	padding: 10px;
+	display: block;
+	width: 80%;
+	margin: auto;
 }
-.wp_cta_container #cta-link {
+#cta_container #cta-link {
   text-decoration: none;
 }
-.wp_cta_container .button {
+#cta_container .button {
   display: block;
   cursor: pointer;
   width: 200px;
@@ -67,12 +68,12 @@ margin: auto;
   transition: all 0.4s ease-in-out;
 }
 
-.wp_cta_container  .button:hover {
+#cta_container  .button:hover {
   background: #c12424;
   border-bottom: 3px solid #db3d3d;
 }
 
-.wp_cta_container  .clicked {
+#cta_container  .clicked {
   transform: rotateY(-80deg);
 }
 
@@ -83,7 +84,7 @@ margin: auto;
 	padding-left:20px;
 	padding-right:20px;
 	color: #{{content-text-color}};
-	text-align: center;
+	text-align: {{content-alignment}};
 }
 .cta_content h1,.cta_content h2,.cta_content h3,.cta_content h4,.cta_content h5,.cta_content h6 {
 	color: #{{content-text-color}};
@@ -95,6 +96,7 @@ margin: auto;
 	padding-bottom: 5px;
 }
 .cta_button, #cta_container input[type="button"], #cta_container button[type="submit"], #cta_container input[type="submit"] {
+	text-align:center;
 	background: #{{submit-button-color}};
 	border-bottom: 3px solid {{submit-button-color|brightness(55)}};
 	color: #{{submit-button-text-color}};
@@ -119,12 +121,14 @@ margin: auto;
 	background: {{submit-button-color|brightness(55)}};
 	border-bottom: 3px solid #{{submit-button-color}};
 }
-.wp_cta_container  h1#main-headline {
+#cta_container  h1#main-headline {
 	color: #{{headline-text-color}};
 	margin-top: 0px;
 	padding-top: 10px;
 	line-height: 36px;
 	margin-bottom: 10px;
+	font-weight: 300;
+	font-size:20px;
 }
 #cta_container a {
 	text-decoration: none;
