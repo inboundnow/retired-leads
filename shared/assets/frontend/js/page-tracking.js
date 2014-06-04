ce23be9b59251c34c2378c92de028d38bc629f82
Inbound Now <david@inboundnow.com>
2014-06-03 20:14:26 -0700

    update tracking js

--- shared/assets/frontend/js/page-tracking.js
+++ shared/assets/frontend/js/page-tracking.js
@@ -1,22 +1,3 @@
-/* Console.log fix for old browsers */
-(function() {
-  if (!window.console) {
-    window.console = {};
-  }
-  // union of Chrome, FF, IE, and Safari console methods
-  var m = [
-    "log", "info", "warn", "error", "debug", "trace", "dir", "group",
-    "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
-    "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
-  ];
-  // define undefined methods as noops to prevent errors
-  for (var i = 0; i < m.length; i++) {
-    if (!window.console[m[i]]) {
-      window.console[m[i]] = function() {};
-    }
-  }
-})();
-
 /**
  * Lead Tracking JS
  * http://www.inboundnow.com
@@ -32,9 +13,25 @@ var InboundAnalytics = (function () {
 
    var App = {
      init: function () {
+          this.polyFills();
           InboundAnalytics.PageTracking.StorePageView();
           InboundAnalytics.Events.loadEvents();
           InboundAnalytics.Utils.init();
+     },
+     polyFills: function() {
+          /* Console.log fix for old browsers */
+          if (!window.console) { window.console = {}; }
+          var m = [
+            "log", "info", "warn", "error", "debug", "trace", "dir", "group",
+            "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+            "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+          ];
+          // define undefined methods as noops to prevent errors
+          for (var i = 0; i < m.length; i++) {
+            if (!window.console[m[i]]) {
+              window.console[m[i]] = function() {};
+            }
+          }
      },
      /* Debugger Function toggled by var debugMode */
      debug: function(msg,callback){
