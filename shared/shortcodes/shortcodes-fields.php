4d145ba340bf775ed53e49e51c6be3e4dd63f787
Hudson Atwell <hudson.atwell@gmail.com>
2014-06-30 17:29:18 -0500

    Renaming class InboundShortcodes -> Inbound_Shortcodes & InboundShortcodesFields -> Inbound_Shortcodes_Fields

--- shared/shortcodes/shortcodes-fields.php
+++ shared/shortcodes/shortcodes-fields.php
@@ -1,5 +1,6 @@
 <?php
-if (!class_exists('Inbound_Shortcodes_Fields')) {
+
+if ( !class_exists('Inbound_Shortcodes_Fields') ) {
 
 	/* 	Include wp-load
 	* 	----------------------------------------------------- */
@@ -8,8 +9,6 @@ if (!class_exists('Inbound_Shortcodes_Fields')) {
 	$path_to_file = explode( 'wp-content', __FILE__ );
 	$path_to_wp = $path_to_file[0];
 	require_once( $path_to_wp . '/wp-load.php' );
-
-
 
 	/* 	The Class
 	* 	----------------------------------------------------- */
