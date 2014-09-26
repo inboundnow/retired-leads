7cc2212b51be84d90ffd914ec9faddb0113a7467
Inbound Now <david@inboundnow.com>
2014-09-25 19:16:29 -0700

    style updates

--- js/wpl.admin.edit.js
+++ js/wpl.admin.edit.js
@@ -171,6 +171,12 @@ jQuery(document).ready(function($) {
 	});
 
 	jQuery("#show-hidden-fields").click(function() {
+		var $this = $(this);
+		if($this.text() === "Show Empty Fields") {
+			$this.text("Hide Empty Fields");
+		} else {
+			$this.text("Show Empty Fields");
+		}
 		jQuery(".hidden-lead-fields").toggle();
 		jQuery("#add-notes").hide();
 	});
