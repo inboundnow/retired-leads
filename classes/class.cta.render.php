99d1efac91b56a8a1b5ca8b59fff51cbdc17ffdd
Inbound Now <david@inboundnow.com>
2014-10-07 18:10:55 -0700

    add back in template token functions

--- classes/class.cta.render.php
+++ classes/class.cta.render.php
@@ -10,7 +10,6 @@
 
 */
 
-
 if ( !class_exists( 'CTA_Render' ) ) {
 
 
@@ -1348,6 +1347,97 @@ if ( !class_exists( 'CTA_Render' ) ) {
 		}
 	}
 }
+/*
+Util functions for token parser.
+usage: {{token|function_name}}
+usage: {{token|color}} run inbound_template_color on token
+ */
+if (!function_exists('inbound_template_color')) {
+	function inbound_template_color($args){
+		$prefix = "#";
+		if (is_array($args)){
+			$color = $args[0];
+		} else {
+			$color = $args;
+		}
+		if(preg_match("/rbg/", $color)) {
+			$prefix = "";
+		}
+
+		return $prefix . $color;
+	}
+}
+if (!function_exists('inbound_template_brightness')) {
+	function inbound_template_brightness($args){
+
+			$hex_color = $args[0];
+			$hue = intval($args[1]);
+
+			$format = 'hex';
+			if (strpos($hex_color,'#') !== false) {
+			    $input = $hex_color;
+			} else {
+				$input = "#" . $hex_color;
+			}
+
+			$col = Array(
+			    hexdec(substr($input,1,2)),
+			    hexdec(substr($input,3,2)),
+			    hexdec(substr($input,5,2))
+			);
+
+			$color_scheme_array =
+			array(
+					100 => array( $col[0]/4, $col[1]/4, $col[2]/4),
+					95 => array( $col[0]/3, $col[1]/3, $col[2]/3),
+					90 => array( $col[0]/2.7, $col[1]/2.7, $col[2]/2.7),
+					85 => array( $col[0]/2.5, $col[1]/2.5, $col[2]/2.5),
+					80 => array( $col[0]/2.2, $col[1]/2.2, $col[2]/2.2),
+					75 => array( $col[0]/2, $col[1]/2, $col[2]/2),
+					70 => array( $col[0]/1.7, $col[1]/1.7, $col[2]/1.7),
+					65 => array( $col[0]/1.5, $col[1]/1.5, $col[2]/1.5),
+					60 => array( $col[0]/1.3,$col[1]/1.3,$col[2]/1.3),
+					55 => array( $col[0]/1.1,$col[1]/1.1,$col[2]/1.1),
+					50 => array( $col[0],$col[1],$col[2]),
+					45 => array( 255-(255-$col[0])/1.1, 255-(255-$col[1])/1.1, 255-(255-$col[2])/1.1),
+					40 => array( 255-(255-$col[0])/1.3, 255-(255-$col[1])/1.3, 255-(255-$col[2])/1.3),
+					35 => array( 255-(255-$col[0])/1.5, 255-(255-$col[1])/1.5, 255-(255-$col[2])/1.5),
+					30 => array( 255-(255-$col[0])/1.7, 255-(255-$col[1])/1.7, 255-(255-$col[2])/1.7),
+					25 => array( 255-(255-$col[0])/2, 255-(255-$col[1])/2, 255-(255-$col[2])/2),
+					20 => array( 255-(255-$col[0])/2.2, 255-(255-$col[1])/2.2, 255-(255-$col[2])/2.2),
+					15 => array( 255-(255-$col[0])/3, 255-(255-$col[1])/2.7, 255-(255-$col[2])/3),
+					10 => array(255-(255-$col[0])/5, 255-(255-$col[1])/5, 255-(255-$col[2])/5),
+					5 => array(255-(255-$col[0])/10, 255-(255-$col[1])/10, 255-(255-$col[2])/10),
+					0 => array(255-(255-$col[0])/15, 255-(255-$col[1])/15, 255-(255-$col[2])/15)
+					);
+
+			($format === 'hex') ? $sign = "#" : $sign = '';
+			$return_scheme = array();
+			foreach ($color_scheme_array as $key => $val) {
+
+				$each_color_return =	$sign.sprintf("%02X%02X%02X", $val[0], $val[1], $val[2]);
+			    $return_scheme[$key] = $each_color_return;
+
+			}
+				//return $closest;
+				if(isset($_GET['color_scheme'])) {
+					foreach ($return_scheme as $key => $hex_value) {
+						echo "<div style='background:$hex_value; display:block; width:100%;'>$key</div>";
+					}
+				}
+
+				$new_color = $return_scheme[$hue];
+				if (strpos($new_color,'#') !== false) {
+				    $return_color = $new_color;
+				} else {
+					$return_color = "#" . $new_color;
+				}
+
+				return $return_color;
+
+	}
+}
+
 
 function wp_cta_check_active()
 {
