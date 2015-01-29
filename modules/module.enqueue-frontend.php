a5efbf1c71e470db7e1381616cd40b40e4bd9de4
Hudson Atwell <atwell.publishing@gmail.com>
2015-01-29 17:33:53 -0600

    remove form prepopulation code left in from landing pages framwork.

--- modules/module.enqueue-frontend.php
+++ modules/module.enqueue-frontend.php
@@ -7,22 +7,5 @@ function wpleads_enqueuescripts_header() {
 
 	$post_type = isset($post) ? get_post_type( $post ) : null;
 
-	// Load Tracking Scripts
-	if($post_type != "wp-call-to-action") {
 
-		/* load jquery */
-		wp_enqueue_script('jquery');
-
-		// Load form pre-population
-		$form_prepopulation = get_option( 'wpl-main-form-prepopulation' , 1); // Check lead settings
-		$lp_form_prepopulation = get_option( 'lp-main-landing-page-prepopulate-forms' , 1);
-		if ($lp_form_prepopulation === "1") {
-			$form_prepopulation = "1";
-		}
-
-		if ($form_prepopulation === "1") {
-
-		}
-
-	}
 }
