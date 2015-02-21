477dd270574ff7e391be7b0d2b40dbd44279dbb3
Inbound Now <david@inboundnow.com>
2015-02-20 18:26:24 -0800

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,8 @@
+/* for main build processes we use gulp see gulpfile.js */
 module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: [	'shared/assets/frontend/js/analytics/analytics.js',
-        		'shared/assets/frontend/js/analytics/*.js',
-        		'shared/assets/frontend/js/analytics/analytics.init.js'
-        ],
-        dest: 'shared/assets/frontend/js/analytics.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    jshint: {
-      files: ['Gruntfile.js', '/shared/assets/frontend/js/analytics/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint', 'qunit']
-    },
     wp_readme_to_markdown: {
       cta: {
           files: {
@@ -52,14 +12,7 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
-
-  grunt.registerTask('test', ['jshint', 'qunit']);
 
   grunt.registerTask('default', ['wp_readme_to_markdown']);
 
