7614c73cbbdf3779cff4a5a1e7b9c4a5f56034d9
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-21 17:55:15 -0500

    refector load shared call. https://github.com/inboundnow/leads/issues/114

--- leads.php
+++ leads.php
@@ -90,14 +90,12 @@ if ( ! class_exists( 'Inbound_Leads_Plugin' ) ) {
 		*  Load Shared Files
 		*/
 		private static function load_shared_files() {
-			if (!defined('INBOUND_PRO_CURRENT_VERSION')) {
-				if (file_exists('shared/classes/class.load-shared.php')) {
-					require_once('shared/classes/class.load-shared.php');
-				} else {
-					require_once(WP_PLUGIN_DIR . '/_inbound-pro/core/shared/classes/class.load-shared.php');
-				}
-				add_action( 'plugins_loaded', array( 'Inbound_Load_Shared' , 'init') , 3 );
-			}
+            if (defined('INBOUND_PRO_PATH')) {
+                include_once( INBOUND_PRO_PATH . 'core/shared/classes/class.load-shared.php' );
+            } else {
+                require_once('shared/classes/class.load-shared.php');
+            }
+            add_action( 'plugins_loaded', array( 'Inbound_Load_Shared' , 'init') , 1 );
 		}
 
 
