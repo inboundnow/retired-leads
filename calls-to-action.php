ab0b8a1905a6626dcc38f5fb63c765e7d36aaa7f
David Wells <david@inboundnow.com>
2015-06-10 09:23:15 -0700

    Security Patch

--- calls-to-action.php
+++ calls-to-action.php
@@ -3,7 +3,7 @@
 Plugin Name: Calls to Action
 Plugin URI: http://www.inboundnow.com/cta/
 Description: Display Targeted Calls to Action on your WordPress site.
-Version: 2.3.9
+Version: 2.4.0
 Author: InboundNow
 Author URI: http://www.inboundnow.com/
 Text Domain: cta
