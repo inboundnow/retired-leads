dbad80d41e081bd8c172e49458874c2bcb90dff3
Hudson Atwell <hudson.atwell@gmail.com>
2015-08-31 13:26:54 -0500

    Creating a shared functions file and adding inbound_get_parent_directory to it. https://github.com/inboundnow/landing-pages/issues/220

--- shared/classes/class.load-shared.php
+++ shared/classes/class.load-shared.php
@@ -55,10 +55,11 @@ if (!class_exists('Inbound_Load_Shared')) {
 			include_once( INBOUNDNOW_SHARED_PATH . 'classes/class.lead-storage.php');
 			include_once( INBOUNDNOW_SHARED_PATH . 'classes/class.ajax.php');
 			include_once( INBOUNDNOW_SHARED_PATH . 'classes/class.inbound-api.php');
-			include_once( INBOUNDNOW_SHARED_PATH . 'shortcodes/inbound-shortcodes.php');
-			include_once( INBOUNDNOW_SHARED_PATH . 'legacy/functions.php');
-			include_once( INBOUNDNOW_SHARED_PATH . 'assets/assets.loader.class.php');
 			include_once( INBOUNDNOW_SHARED_PATH . 'classes/class.inbound-customizer.php');
+			include_once( INBOUNDNOW_SHARED_PATH . 'assets/assets.loader.class.php');
+			include_once( INBOUNDNOW_SHARED_PATH . 'shortcodes/inbound-shortcodes.php');
+			include_once( INBOUNDNOW_SHARED_PATH . 'functions/legacy.php');
+			include_once( INBOUNDNOW_SHARED_PATH . 'functions/shared.php');
 
 			/* load admin only */
 			if (is_admin()) {
