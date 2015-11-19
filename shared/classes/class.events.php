7fe40cfbd3fbff844e11a144ca639b0b8cfe1dad
Hudson Atwell <hudson@inboundnow.com>
2015-11-19 17:00:37 -0600

    Adding method to get email clicks from event database https://github.com/inboundnow/leads/issues/143

--- shared/classes/class.events.php
+++ shared/classes/class.events.php
@@ -224,6 +224,20 @@ class Inbound_Events {
     }
 
     /**
+     * Get all email click events related to lead ID
+     */
+    public static function get_email_clicks( $lead_id ){
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . "inbound_events";
+
+        $query = 'SELECT * FROM '.$table_name.' WHERE `lead_id` = "'.$lead_id.'" AND `event_name` = "inbound_email_click" ORDER BY `datetime` DESC';
+        $results = $wpdb->get_results( $query , ARRAY_A );
+
+        return $results;
+    }
+
+    /**
      * Get all unsubscribe events given a lead id
      */
     public static function get_unsubscribes( $lead_id ){
