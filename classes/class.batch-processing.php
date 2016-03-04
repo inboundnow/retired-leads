aded638820f7da5a5d459e06335c1d5f2ccd4d7a
GITUSER <hudson@inboundnow.com>
2016-03-04 16:37:06 -0600

    Committing uncommitted changes

--- classes/class.batch-processing.php
+++ classes/class.batch-processing.php
@@ -77,9 +77,6 @@ class Leads_Batch_Processor {
         echo '<div class="wrap">';
 
         /* run the method */
-
-        self::{$args['method']}($args);
-
         if (version_compare(phpversion(), '7', '<')) {
             self::$args['method']($args);
         } else {
