bcdc41c8f29b297110e31478967dd52cd0e624b0
GITUSER <hudson@inboundnow.com>
2016-07-11 20:28:06 -0500

    Working with shared

--- shared/classes/class.ajax.php
+++ shared/classes/class.ajax.php
@@ -44,12 +44,12 @@ if (!class_exists('Inbound_Ajax')) {
 
 			global $wpdb;
 
-			(isset(	$_POST['wp_lead_id'] )) ? $lead_data['lead_id'] = $_POST['wp_lead_id'] : $lead_data['lead_id'] = '';
-			(isset(	$_POST['nature'] )) ? $lead_data['nature'] = $_POST['nature'] : $lead_data['nature'] = 'non-conversion'; /* what is nature? */
-			(isset(	$_POST['json'] )) ? $lead_data['json'] = addslashes($_POST['json']) : $lead_data['json'] = 0;
-			(isset(	$_POST['wp_lead_uid'] )) ? $lead_data['wp_lead_uid'] = $_POST['wp_lead_uid'] : $lead_data['wp_lead_uid'] = 0;
-			(isset(	$_POST['page_id'] )) ? $lead_data['page_id'] = $_POST['page_id'] : $lead_data['page_id'] = 0;
-			(isset(	$_POST['current_url'] )) ? $lead_data['current_url'] = $_POST['current_url'] : $lead_data['current_url'] = 'notfound';
+			$lead_data['lead_id'] = (isset(	$_POST['wp_lead_id'] )) ?  $_POST['wp_lead_id'] : '';
+			$lead_data['nature'] = (isset(	$_POST['nature'] )) ? $_POST['nature'] : 'non-conversion'; /* what is nature? */
+			$lead_data['json'] = (isset( $_POST['json'] )) ? addslashes($_POST['json']) : 0;
+			$lead_data['wp_lead_uid'] =  (isset( $_POST['wp_lead_uid'] )) ? $_POST['wp_lead_uid'] : 0;
+			$lead_data['page_id'] = (isset(	$_POST['page_id'] )) ? $_POST['page_id'] :  0;
+			$lead_data['current_url'] = (isset(	$_POST['current_url'] )) ? $_POST['current_url'] : 'notfound';
 
 
 			$page_views = stripslashes($_POST['page_views']);
